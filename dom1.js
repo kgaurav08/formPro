863a0da38fb17acffe74d3503222f8eed2683230
kgaurav08 <kumargaurav9788@gmail.com>
2023-07-21 13:38:28 +0530

    release

--- dom1.js
+++ dom1.js
@@ -4,4 +4,14 @@ items[2].style.fontWeight = 'green';
 items[0].style.fontWeight = 'bold';
 items[1].style.fontWeight = 'bold';
 items[2].style.fontWeight = 'bold';
-items[3].style.fontWeight = 'bold';
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+li[4].textContent = 'Hello 5';
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor = '#f4f4f4';
+}
